373667e96508c7873eff0471b5ab1233cb70c7aa
Bavagowri <bavagowri.s1@gmail.com>
2025-12-08 11:23:58 +0530

    file moved

--- src/services/sportmonks.js
+++ src/services/sportmonks.js
@@ -1,5 +1,5 @@
 // src/services/sportmonks.js
-const axios = require("axios");
+const axios = require("../../node_modules/axios/index.d.cts");
 const NodeCache = require("node-cache");
 const { SPORTMONKS_API_TOKEN } = require("../config");
 

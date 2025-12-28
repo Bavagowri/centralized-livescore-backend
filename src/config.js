2a4269e78895f8a3c5c04cd712aea5e49e7a09e8
Bavagowri <bavagowri.s1@gmail.com>
2025-12-28 11:35:22 +0530

    update

--- src/config.js
+++
@@ -1,6 +0,0 @@
-import dotenv from "dotenv";
-dotenv.config();
-
-export const PORT = process.env.PORT || 4000;
-export const SPORTMONKS_API_TOKEN = process.env.SPORTMONKS_API_TOKEN;
-export const INTERNAL_API_KEY = process.env.INTERNAL_API_KEY;
